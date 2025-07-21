21e96c7f4faa6769a8c585a8077fb62c9c489f47
Cristhofer Leonardo Quispe Mamani <144476859+Crsto22@users.noreply.github.com>
2025-07-21 16:04:59 -0500

    Update cache.js

--- src/services/cache.js
+++ src/services/cache.js
@@ -1,8 +1,11 @@
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL || 'redis://default:kXtffHElUGkLOVgnfSVezxsIEetmQxGt@redis.railway.internal:6379', // Fallback para pruebas locales
+  url: process.env.REDIS_URL || 'redis://localhost:6379', // Fallback para pruebas locales
 });
+
+// Log para depurar la URL utilizada
+console.log('Intentando conectar a Redis con URL:', process.env.REDIS_URL || 'redis://localhost:6379');
 
 // Manejo de errores detallado
 redisClient.on('error', (err) => {
