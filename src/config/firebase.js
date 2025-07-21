ceaab06f63c21180524509ff06fb5dcb31097c91
Cristhofer Leonardo Quispe Mamani <144476859+Crsto22@users.noreply.github.com>
2025-07-21 16:35:22 -0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,17 +3,17 @@ const { getFirestore } = require('firebase/firestore');
 
 // Configuración de Firebase para el proyecto bodeguitavanesa
 const firebaseConfig = {
-  apiKey: 'AIzaSyCx8JzJ_eVrArMUDi-GkZ9FpgKM6mnZEV8',
+  apiKey: 'AIzaSyCx8JzJ_eVrArMUDi-GkZ9FpgKM6mnZEV8', // Considera usar variables de entorno para esto también
   authDomain: 'bodeguitavanesa.firebaseapp.com',
   projectId: 'bodeguitavanesa',
-  storageBucket: 'bodeguitavanesa.firebasestorage.app',
+  storageBucket: 'bodeguitavanesa.appspot.com', // Corregido: suele ser .appspot.com
   messagingSenderId: '734082784317',
   appId: '1:734082784317:web:c196d30c1d8538a14ede54',
   measurementId: 'G-169FNDEJ8N',
 };
 
 // Inicializar Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app, 'negociovanesa');
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp); // No necesitas el segundo argumento 'negociovanesa' aquí
 
 module.exports = { db };
